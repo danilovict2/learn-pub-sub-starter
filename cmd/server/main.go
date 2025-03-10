e408fdc2450b981d41da2b4e48d84d82ced2cd6c
Danilo <danilosindric@gmail.com>
2025-03-10 16:17:43 +0100

    Update: connect to rabbitmq

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+
+	ampq "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
-	fmt.Println("Starting Peril server...")
+	const rabbitConnString = "amqp://guest:guest@localhost:5672/"
+	conn, err := ampq.Dial(rabbitConnString)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
+	fmt.Println("Peril game server connected to RabbitMQ!")
+	
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)	
+	<-signalChan
 }
