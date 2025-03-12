9a0d1d3466cb6584a680ed37907ae433cb249bc0
Danilo <danilosindric@gmail.com>
2025-03-12 16:26:14 +0100

    Update: add game loop

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,9 +3,8 @@ package main
 import (
 	"fmt"
 	"log"
-	"os"
-	"os/signal"
 
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
 
@@ -22,18 +21,40 @@ func main() {
 
 	fmt.Println("Peril game server connected to RabbitMQ!")
 
+	gamelogic.PrintServerHelp()
+Loop:
+	for {
+		words := gamelogic.GetInput()
+		if len(words) == 0 {
+			continue
+		}
+
+		switch words[0] {
+		case "pause":
+			log.Println("Pausing")
+			publishPause(conn, true)
+		case "resume":
+			log.Println("Resuming")
+			publishPause(conn, false)
+		case "quit":
+			log.Println("Quitting")
+			break Loop
+		default:
+			log.Println("Don't understand the command:", words[0])
+		
+		}
+	}
+}
+
+func publishPause(conn *ampq.Connection, isPaused bool) {
 	ampqChan, err := conn.Channel()
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	if err := pubsub.PublishJSON(ampqChan, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{
-		IsPaused: false,
+		IsPaused: isPaused,
 	}); err != nil {
 		log.Fatal(err)
 	}
-
-	signalChan := make(chan os.Signal, 1)
-	signal.Notify(signalChan, os.Interrupt)
-	<-signalChan
 }
